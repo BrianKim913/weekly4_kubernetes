8a2f5a34f6606a23b0b5fe41bfae835cb0e47955
Brian Kim <bumsookim333@naver.com>
2025-02-21 14:51:15 +0900

    add logger

--- src/main/java/com/prac/backend/BackendApplication.java
+++ src/main/java/com/prac/backend/BackendApplication.java
@@ -4,28 +4,31 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class BackendApplication {
-
+	private static final Logger logger = LoggerFactory.getLogger(BackendApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
 	@GetMapping("/")
 	public String helloKubernates() {
-		System.out.print("request");
+		logger.info("Request received");
 		return "Hello, kubernates prac";
 	}
 
 	@GetMapping("/loadtest")
 	public String loadTest() {
-		System.out.print("request");
+		logger.info("Load test Request received");
 		long result =0;
 		for (int i = 0; i < 1000000; i++) {
 			result += (long) Math.sqrt(i);
 		}
+		logger.info("Load test result :" + result);
 		return "로드테스트"+ result ;
 	}
 }
